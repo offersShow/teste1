90902096e150af12d1d3b25381afb03d40bd6a5d
offersShow <azevedoadeilson4@gmail.com>
2025-07-08 05:19:32 -0300

    Update servidor_codigo.py

--- servidor_codigo.py
+++ servidor_codigo.py
@@ -26,5 +26,6 @@ if __name__ == '__main__':
     @app.route('/ping')
     def ping():
         return "pong", 200
-    app.run(host='127.0.0.1', port=5050)
+    port = int(os.environ.get("PORT", 5050))
+    app.run(host='0.0.0.0', port=port)
 
